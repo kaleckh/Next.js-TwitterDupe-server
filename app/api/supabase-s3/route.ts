d2e115e9a7c761945ce861774063ae138de448ae
kaleckh <kaleckh@gmail.com>
2024-09-13 16:46:40 -0600

    23 image buckets

--- app/api/supabase-s3/route.ts
+++ app/api/supabase-s3/route.ts
@@ -1,11 +1,30 @@
-import { S3Client } from '@aws-sdk/client-s3';
+import { createClient } from '@supabase/supabase-js';
+import FormData from 'form-data';
 
-const client = new S3Client({
-  forcePathStyle: true,
-  region: 'project_region',
-  endpoint: 'https://project_ref.supabase.co/storage/v1/s3',
-  credentials: {
-    accessKeyId: 'your_access_key_id',
-    secretAccessKey: 'your_secret_access_key',
-  }
-})
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase URL or Anon Key in environment variables.');
+}
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+import { NextResponse, NextRequest } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  let requestData = await req.formData();
+  const file = requestData.get('image'); 
+  console.log(file,'this is the file')
+  // const { data, error } = await supabase.storage.from('TwitterDupe').upload('profile-images/testing.jps', file)
+  // console.log(data, 'data')
+}
+
+
+

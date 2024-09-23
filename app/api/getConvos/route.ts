e9ff88e1f2ba4f8040501fc2fb6ab52094864acd
kaleckh <kaleckh@gmail.com>
2024-09-23 15:52:22 -0600

    changed the route

--- app/api/getConvos/route.ts
+++ app/api/getConvos/route.ts
@@ -6,19 +6,15 @@ const prisma = new PrismaClient()
 export async function GET(req: NextRequest) {
     try {
         const email = req.nextUrl.searchParams.get('email')        
+        
         const test = await prisma.conversation.findMany({
-            where: {
-                OR: [
-                    {
-                        me: email || ''
-                    },
-                    {
-                        recipient: email,
-
-                    }
-                ]
+            where:
+            {
+                me: email || ''
             }
-        })        
+
+        })
+        console.log(test, 'test')
         return NextResponse.json({ Posts: test });
     } catch (error) {
         console.log(error)

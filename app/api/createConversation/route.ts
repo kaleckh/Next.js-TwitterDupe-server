810fd0208dd27982e9399072d4df8028277182da
Noah Clark <noahammon00@gmail.com>
2024-09-25 22:35:59 -0600

    fixed schema some more for messages and conversations as well as fixed the create conversation route accordingly

--- app/api/createConversation/route.ts
+++ app/api/createConversation/route.ts
@@ -1,35 +1,52 @@
-import { PrismaClient } from '@prisma/client'
-import { NextResponse, NextRequest } from 'next/server'
+import { PrismaClient } from "@prisma/client";
+import { NextResponse, NextRequest } from "next/server";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
-    const data = await req.json()
-    console.log(data, 'this is data')
+  const data = await req.json();
+  console.log(data, "this is data");
 
-    try {
-        const updateLikes = await prisma.conversation?.create({
-            data: {
-                me: data.me,
-                roomName: data.roomName,
-                recipient: data.recipient,
-                date: new Date()
-            }
-        })
-        await prisma.message.create({
-            data: {
-                conversationId: updateLikes.id,
-                message: data.messages.message,
-                status: 'Delivered',
-                userName: data.messages.userName,
-                date: new Date(),
-                recipient: data.recipient
-            }
-        })
+  try {
+    const newConversation = await prisma.conversation.create({
+      data: {
+        date: new Date(),
+        users: {
+          create: [
+            {
+              user: {
+                connect: {
+                  id: data.myId,
+                },
+              },
+            },
+            {
+              user: {
+                connect: {
+                  id: data.recipientId,
+                },
+              },
+            },
+          ],
+        },
+      },
+    });
+    await prisma.message.create({
+      data: {
+        conversationId: newConversation.id,
+        message: data.message,
+        status: "Delivered",
+        userId: data.myId,
+        date: new Date(),
+      },
+    });
 
-        return NextResponse.json({ update: updateLikes })
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
-    }
+    return NextResponse.json({ update: newConversation });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }

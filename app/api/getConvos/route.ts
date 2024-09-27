31dccc6632830096b28f41b6ce2bc79c52084f32
Noah Clark <noahammon00@gmail.com>
2024-09-26 20:57:02 -0600

    edited getconvos route to take in user id and grab the user with conversations included

--- app/api/getConvos/route.ts
+++ app/api/getConvos/route.ts
@@ -5,19 +5,23 @@ const prisma = new PrismaClient()
 
 export async function GET(req: NextRequest) {
     try {
-        const email = req.nextUrl.searchParams.get('email')        
+        const id = req.nextUrl.searchParams.get('id')        
         
         const test = await prisma.user.findUnique({
             where:
             {
-                email: email || ''
+                id: id || ''
             },
             include: {
-                conversations: true
+                conversations: {
+                    include: {
+                        user: true
+                    }
+                }
             }
         })
         console.log(test, 'test')
-        return NextResponse.json({ Posts: test });
+        return NextResponse.json({ user: test });
     } catch (error) {
         console.log(error)
     }

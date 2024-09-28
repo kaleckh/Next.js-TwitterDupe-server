e9d2e58099f6f814d8003bccc2f4796127f1c09f
Noah Clark <noahammon00@gmail.com>
2024-09-27 19:54:38 -0600

    got getConvos route to send all needed information for the messages page on the frontend

--- app/api/getConvos/route.ts
+++ app/api/getConvos/route.ts
@@ -5,23 +5,39 @@ const prisma = new PrismaClient()
 
 export async function GET(req: NextRequest) {
     try {
-        const id = req.nextUrl.searchParams.get('id')        
+        const id = req.nextUrl.searchParams.get('id')   
+        if (!id) throw new Error('no id')     
         
-        const test = await prisma.user.findUnique({
-            where:
-            {
-                id: id || ''
+        const test = await prisma.conversation.findMany({
+            where: {
+                users: {
+                    some: {
+                        user: {
+                            id
+                        }
+                    }
+                }
             },
             include: {
-                conversations: {
+                users: {
+                    where: {
+                        userId: {
+                            not: id
+                        }
+                    },
                     include: {
                         user: true
+                    }
+                },
+                messages: {
+                    orderBy: {
+                        date: 'asc'
                     }
                 }
             }
         })
-        console.log(test, 'test')
-        return NextResponse.json({ user: test });
+        console.log(test, 'these are the conversations')
+        return NextResponse.json({ conversations: test });
     } catch (error) {
         console.log(error)
     }

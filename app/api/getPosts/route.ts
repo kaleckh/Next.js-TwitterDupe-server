78adecea6922fee79b821bcb9bed7ea4678d94d3
kaleckh <kaleckh@gmail.com>
2024-09-30 12:47:34 -0600

    added cors config stuff

--- app/api/getPosts/route.ts
+++ app/api/getPosts/route.ts
@@ -1,17 +1,31 @@
 import { PrismaClient } from '@prisma/client';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
-export async function GET() {
-    console.log('hitting get for you posts')
-    try {        
+export async function GET(req: NextRequest) {
+    const origin = req.headers.get('origin');
+
+    // Add CORS headers to allow specific domains or all domains
+    const headers = new Headers();
+    headers.set('Access-Control-Allow-Origin', origin || '*'); // Allow all domains or restrict to specific origin
+    headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Allow specific methods
+    headers.set('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        // Handle preflight requests
+        return new Response(null, {
+            headers,
+            status: 204,
+        });
+    }
+    try {
         const posts = await prisma.post.findMany({
             orderBy: {
                 date: 'desc',
             },
             include: {
-                comments: true, 
+                comments: true,
                 owner: true
             },
         });

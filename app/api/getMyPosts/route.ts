457643e2bd97a087b358d8e611809d72b54b3574
Noah Clark <noahammon00@gmail.com>
2024-09-30 20:28:00 -0600

    changed route to also get reposts from a specific user

--- app/api/getMyPosts/route.ts
+++ app/api/getMyPosts/route.ts
@@ -5,6 +5,8 @@ const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
     const email = req.nextUrl.searchParams.get('email');
+    const id = req.nextUrl.searchParams.get('id')
+    if(!id) throw new Error()
 
     try {
         const posts = await prisma.post.findMany({
@@ -17,21 +19,32 @@ export async function GET(req: NextRequest) {
             include: {
                 comments: true,
                 owner: true,
-                // reposts: {
-                //     include: {
-                //         post: {  // Include the full reposted post details
-                //             include: {
-                //                 comments: true,  // Optionally include comments on the reposted post                                
-                //             },
-                //         },
-                //         user: true,  // Include the user who made the repost
-                //     },
-                // },
+                reposts: {
+                    include: {
+                        post: {  // Include the full reposted post details
+                            include: {
+                                comments: true,  // Optionally include comments on the reposted post                                
+                            },
+                        },
+                        user: true,  // Include the user who made the repost
+                    },
+                },
             },
         });
 
-        console.log(posts, "this is get all posts info");
-        return NextResponse.json(posts);
+        const reposts = await prisma.repost.findMany({
+            where: {
+                userId: id
+            },
+            orderBy: {
+                date: 'desc'
+            },
+            include: {
+                post: true
+            }
+        })
+        console.log(reposts, 'repost info')
+        return NextResponse.json({posts, reposts});
     } catch (error) {
         console.log(error);
         return NextResponse.json({ error: 'An error occurred' }, { status: 500 });

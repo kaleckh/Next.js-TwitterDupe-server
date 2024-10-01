16bc20e309b3c6a5bf6d187428d990864679d901
kaleckh <kaleckh@gmail.com>
2024-10-01 15:20:35 -0600

    reposts added to return statement on social mobile server

--- app/api/getPosts/route.ts
+++ app/api/getPosts/route.ts
@@ -3,22 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
 
-export async function GET(req: NextRequest) {
-    // const origin = req.headers.get('origin');
-
-    // // Add CORS headers to allow specific domains or all domains
-    // const headers = new Headers();
-    // headers.set('Access-Control-Allow-Origin', origin || '*'); // Allow all domains or restrict to specific origin
-    // headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Allow specific methods
-    // headers.set('Access-Control-Allow-Headers', 'Content-Type');
-
-    if (req.method === 'OPTIONS') {
-        // Handle preflight requests
-        return new Response(null, {
-            // headers,
-            status: 204,
-        });
-    }
+export async function GET(req: NextRequest) { 
     try {
         const posts = await prisma.post.findMany({
             orderBy: {
@@ -27,7 +12,7 @@ export async function GET(req: NextRequest) {
             include: {
                 comments: true,
                 owner: true,
-                // reposts: true
+                reposts: true
             },
         });
         console.log(posts, 'this is post info');

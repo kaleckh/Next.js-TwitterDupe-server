cb3e24b9ce316418d1367dc1ae144c1e2ce0ae6e
Noah Clark <noahammon00@gmail.com>
2024-10-01 22:46:51 -0600

    get posts route now includes reposts and sorts the posts and reposts together by date

--- app/api/getPosts/route.ts
+++ app/api/getPosts/route.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Repost, Post } from '@prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
 
 const prisma = new PrismaClient();
@@ -15,8 +15,27 @@ export async function GET(req: NextRequest) {
                 reposts: true
             },
         });
-        console.log(posts, 'this is post info');
-        return NextResponse.json({ Posts: posts });
+
+        const reposts = await prisma.repost.findMany({
+            orderBy: {
+                date: 'desc'
+            },
+            include: {
+                user: true,
+                post: {
+                    include: {
+                        comments: true,
+                        owner: true,
+                        reposts: true
+                    }
+                }
+            }
+        })
+        //@ts-ignore
+        const query = [...posts, ...reposts].sort((a, b) => new Date(b.date) - new Date(a.date))
+
+        console.log(query, 'this is post info');
+        return NextResponse.json({ Posts: query });
     } catch (error) {
         console.log(error);
         return NextResponse.json({ error: 'Failed to fetch posts' }, { status: 500 });

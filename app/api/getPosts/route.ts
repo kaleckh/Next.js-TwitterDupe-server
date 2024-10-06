7ab119df9a2bf259539b4209c3ee22a932cf2653
Noah Clark <noahammon00@gmail.com>
2024-10-06 16:43:12 -0600

    made it so posts would be filtered by who u follow

--- app/api/getPosts/route.ts
+++ app/api/getPosts/route.ts
@@ -4,46 +4,108 @@ import { NextRequest, NextResponse } from "next/server";
 const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
-  try {
-    const posts = await prisma.post.findMany({
-      orderBy: {
-        date: "desc",
-      },
-      include: {
-        comments: true,
-        owner: true,
-        reposts: true,
-      },
-    });
-
-    const reposts = await prisma.repost.findMany({
-      orderBy: {
-        date: "desc",
-      },
-      include: {
-        user: true,
-        post: {
-          include: {
-            comments: true,
-            owner: true,
-            reposts: true,
+  const userId = req.nextUrl.searchParams.get("id")
+  if(!userId) {
+    try {
+      const posts = await prisma.post.findMany({
+        orderBy: {
+          date: "desc",
+        },
+        include: {
+          comments: true,
+          owner: true,
+          reposts: true,
+        },
+      });
+  
+      const reposts = await prisma.repost.findMany({
+        orderBy: {
+          date: "desc",
+        },
+        include: {
+          user: true,
+          post: {
+            include: {
+              comments: true,
+              owner: true,
+              reposts: true,
+            },
           },
         },
-      },
-    });
-    //@ts-ignore
-    const query = [...posts, ...reposts].sort(
-      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-    );
-    
+      });
+      //@ts-ignore
+      const query = [...posts, ...reposts].sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      );
+      
+  
+      console.log(query, "this is post info");
+      return NextResponse.json({ Posts: query });
+    } catch (error) {
+      console.log(error);
+      return NextResponse.json(
+        { error: "Failed to fetch posts" },
+        { status: 500 },
+      );
+    }
+  } else {
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    })
 
-    console.log(query, "this is post info");
-    return NextResponse.json({ Posts: query });
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      { error: "Failed to fetch posts" },
-      { status: 500 },
-    );
+    if(!user) throw new Error('user no exist')
+
+    try {
+      const posts = await prisma.post.findMany({
+        where: {
+          owner: {
+            id: { in: [...user.following, userId] }
+          }
+        },
+        orderBy: {
+          date: "desc",
+        },
+        include: {
+          comments: true,
+          owner: true,
+          reposts: true,
+        },
+      });
+  
+      const reposts = await prisma.repost.findMany({
+        where: {
+          userId: {
+            in: [...user.following, userId]
+          }
+        },
+        orderBy: {
+          date: "desc",
+        },
+        include: {
+          user: true,
+          post: {
+            include: {
+              comments: true,
+              owner: true,
+              reposts: true,
+            },
+          },
+        },
+      });
+      //@ts-ignore
+      const query = [...posts, ...reposts].sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+      );
+      
+  
+      console.log(query, "this is post info");
+      return NextResponse.json({ Posts: query });
+    } catch (error) {
+      console.log(error);
+      return NextResponse.json(
+        { error: "Failed to fetch posts" },
+        { status: 500 },
+      );
+    }
   }
 }

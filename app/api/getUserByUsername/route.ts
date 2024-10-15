438fee63171268754116be4c8fbb5a51cc4d40fc
Noah Clark <noahammon00@gmail.com>
2024-10-15 15:00:48 -0600

    get user by username route added

---
+++ app/api/getUserByUsername/route.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse, NextRequest } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET(req: NextRequest) {
+  const username = req.nextUrl.searchParams.get("username")?.toLowerCase();
+  console.log(username, "this is the username");
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        username
+      }
+    });
+    return NextResponse.json({ user });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+  }
+}

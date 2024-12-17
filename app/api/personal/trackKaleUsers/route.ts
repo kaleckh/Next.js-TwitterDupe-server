a74982adf972e8eff5bc9e958763353914ff2011
kaleckh <kaleckh@gmail.com>
2024-12-17 10:35:26 -0700

    staged changes

--- app/api/personal/trackKaleUsers/route.ts
+++ app/api/personal/trackKaleUsers/route.ts
@@ -3,22 +3,26 @@ import { NextResponse, NextRequest } from "next/server";
 const prisma = new PrismaClient();
 export async function POST(req: NextRequest) {
     const data = await req.json();
-    try {        
+    console.log(data, 'this data');
+    try {
+        // Check if the IP already exists in the database
         const existingEntry = await prisma.portfolioData.findUnique({
             where: { ip: data.ip },
         });
         if (existingEntry) {
+            // If IP exists, update the counter by incrementing it
             await prisma.portfolioData.update({
                 where: { ip: data.ip },
                 data: { counter: existingEntry.counter + 1 },
             });
             return NextResponse.json("Counter updated successfully");
-        } else {            
+        } else {
+            // If IP does not exist, create a new entry
             await prisma.portfolioData.create({
                 data: {
                     ip: data.ip,
                     date: new Date(),
-                    counter: 1, 
+                    counter: 1, // Initialize counter at 1 for new entries
                 },
             });
             return NextResponse.json("New IP added successfully");
